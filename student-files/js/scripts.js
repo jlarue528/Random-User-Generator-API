17e8980161294bc9b6a1c9b9d3d3eaf8088c0709
Jennifer LaRue <jennifer@photoshelter.com>
2021-09-28 14:35:21 -0400

    successfully generated 12 users on page

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -16,28 +16,31 @@ fetchUserData('https://randomuser.me/api/?results=12')
    Generate User Profile Gallery
 */
 
-const galleryDisplay = document.getElementById('gallery');
 function generateUserProfile (data) {
-        const image = JSON.stringify(data.results[0].picture.medium);
-        const firstName = data.results[0].name.first;
-        const lastName = data.results[0].name.last;
-        const email = data.results[0].email;
-        const city = data.results[0].location.city;
-        const state = data.results[0].location.state;
-        
-        const  html = `
-        <div class="card">
-           <div class="card-img-container">
-                <img class="card-img" src=${image} alt="profile picture">
+        const results = data.results;
+    
+        for(let i = 0; i < results.length; i++) {
+            const image = JSON.stringify(data.results[i].picture.medium);
+            const firstName = data.results[i].name.first;
+            const lastName = data.results[i].name.last;
+            const email = data.results[i].email;
+            const city = data.results[i].location.city;
+            const state = data.results[i].location.state;
+
+            const  html = `
+            <div class="card">
+               <div class="card-img-container">
+                    <img class="card-img" src=${image} alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+                    <p class="card-text">${email}</p>
+                    <p class="card-text cap">${city}, ${state}</p>
+                </div>
             </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-                <p class="card-text">${email}</p>
-                <p class="card-text cap">${city}, ${state}</p>
-            </div>
-        </div>
-    `
-    console.log(html);
-    galleryDisplay.insertAdjacentHTML('beforeend', html);
+        `
+        const galleryDisplay = document.getElementById('gallery');
+        galleryDisplay.insertAdjacentHTML('beforeend', html);
+        }
 }
 
