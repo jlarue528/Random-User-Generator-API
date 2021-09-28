12025cbd038fccafbd4d0f9ec3a09458aa9320e0
Jennifer LaRue <jennifer@photoshelter.com>
2021-09-28 16:57:27 -0400

    debug generateUserModal function

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -10,7 +10,7 @@ async function fetchUserData(url) {
 }
 
 fetchUserData('https://randomuser.me/api/?results=12')
-    .then(data => generateUserProfile(data))
+    .then(data => generateUserProfile(data));
 
 /*
    Generate User Profile Gallery
@@ -28,19 +28,55 @@ function generateUserProfile (data) {
             const state = data.results[i].location.state;
 
             const  html = `
-            <div class="card">
-               <div class="card-img-container">
-                    <img class="card-img" src=${image} alt="profile picture">
+                <div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src=${image} alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+                        <p class="card-text">${email}</p>
+                        <p class="card-text cap">${city}, ${state}</p>
+                    </div>
                 </div>
-                <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-                    <p class="card-text">${email}</p>
-                    <p class="card-text cap">${city}, ${state}</p>
-                </div>
-            </div>
-        `
-        const galleryDisplay = document.getElementById('gallery');
-        galleryDisplay.insertAdjacentHTML('beforeend', html);
+            `
+            const galleryDisplay = document.getElementById('gallery');
+            galleryDisplay.insertAdjacentHTML('beforeend', html);
         }
 }
 
+/*
+    Generate User Modal
+*/
+
+function generateUserModal () {
+    const bodyElement = document.querySelector('body');
+    const modalDiv = document.createElement('div');
+    bodyElement.appendChild(modalDiv);
+    modalDiv.style.display = 'block';
+    const modalHTML = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                <hr>
+                    <p class="modal-text">(555) 555-5555</p>
+                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                    <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>
+    `
+    modalDiv.insertAdjacentHTML('beforeend', modalHTML);
+}
+
+/*
+    Event Handlers
+*/
+
+const userCards = document.querySelectorAll('.card');
+userCards.onclick = function () {
+    generateUserModal();
+}
